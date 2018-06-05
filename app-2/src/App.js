c1436db9547bbc70a3cd166bd2338114885d6cfe
Rana Mohammad <ranamohammad@ranas-mbp.attlocal.net>
2018-06-05 00:41:48 -0500

    edited

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -3,16 +3,30 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      fruits: [
+        'Apple',
+        'Orange',
+        'Banana',
+        'Pineapple',
+        'Peach',
+        'Mango'
+
+      ]
+    }
+  }
   render() {
+    let fruitsToDisplay = this.state.fruits.map((element, index) => {
+      return (
+        <h2 key={index} >  {element}</h2>
+      )
+    })
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {fruitsToDisplay}
       </div>
     );
   }
--- app-5/src/App.js
+++ app-5/src/App.js
@@ -1,21 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Image from './Image';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Image myImage={"https://i.pinimg.com/originals/77/27/f6/7727f6bad769c9dd82570469a06fc209.jpg"} />
       </div>
     );
   }
 }
 
-export default App;
+export default App
